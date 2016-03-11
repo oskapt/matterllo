a2954b5f0e1a11284d8bc1e47b55c67615c5de44
THEBAULT Julien <julien@thebault.co>
2016-03-11 22:35:56 +0100

    Fix link into card hook

--- matterllo/hook/card.py
+++ matterllo/hook/card.py
@@ -47,42 +47,42 @@ class Hook(object):
     def archiveCard(self, action):
         data = action['data']
         context = {
-            'board_link': data['board']['shortLink'],
+            'card_link': data['card']['shortLink'],
             'card_name': data['card']['name'],
         }
-        payload = u':incoming_envelope: Card archived: "[{card_name}](https://trello.com/b/{board_link})"'
+        payload = u':incoming_envelope: Card archived: "[{card_name}](https://trello.com/c/{card_link})"'
 
         return payload.format(**context)
 
     def unarchiveCard(self, action):
         data = action['data']
         context = {
-            'board_link': data['board']['shortLink'],
+            'card_link': data['card']['shortLink'],
             'card_name': data['card']['name'],
         }
-        payload = u':incoming_envelope: Card unarchived: "[{card_name}](https://trello.com/b/{board_link})"'
+        payload = u':incoming_envelope: Card unarchived: "[{card_name}](https://trello.com/c/{card_link})"'
 
         return payload.format(**context)
 
     def renameCard(self, action):
         data = action['data']
         context = {
-            'board_link': data['board']['shortLink'],
+            'card_link': data['card']['shortLink'],
             'card_name': data['card']['name'],
             'card_old_name': data['old']['name'],
         }
-        payload = u':incoming_envelope: Card renamed from "{card_old_name}" to "[{card_name}](https://trello.com/b/{board_link})"'
+        payload = u':incoming_envelope: Card renamed from "{card_old_name}" to "[{card_name}](https://trello.com/c/{card_link})"'
 
         return payload.format(**context)
 
     def renameCardDesc(self, action):
         data = action['data']
         context = {
-            'board_link': data['board']['shortLink'],
+            'card_link': data['card']['shortLink'],
             'card_name': data['card']['name'],
             'card_desc': data['card']['desc'],
         }
-        payload = u''':incoming_envelope: Card updated: "[{card_name}](https://trello.com/b/{board_link})"
+        payload = u''':incoming_envelope: Card updated: "[{card_name}](https://trello.com/c/{card_link})"
 **Description**: {card_desc}'''
 
         return payload.format(**context)
@@ -90,13 +90,12 @@ class Hook(object):
     def commentCard(self, action):
         data = action['data']
         context = {
-            'board_link': data['board']['shortLink'],
             'card_name': data['card']['name'],
             'card_link': data['card']['shortLink'],
             'member_creator': action['memberCreator']['fullName'],
             'comment': data['text'],
         }
-        payload = u''':incoming_envelope: New comment on card "[{card_name}](https://trello.com/b/{card_link})" by `{member_creator}`
+        payload = u''':incoming_envelope: New comment on card "[{card_name}](https://trello.com/c/{card_link})" by `{member_creator}`
 > {comment}'''
 
         return payload.format(**context)
@@ -108,8 +107,9 @@ class Hook(object):
             'board_name': data['board']['name'],
             'board_link': data['board']['shortLink'],
             'card_name': data['card']['name'],
+            'card_link': data['card']['shortLink'],
         }
-        payload = u':incoming_envelope: Card moved: "[{card_name}](https://trello.com/b/{board_link})" moved from "[{board_name}](https://trello.com/b/{board_link}) to board "{board_target_name}"'
+        payload = u':incoming_envelope: Card moved: "[{card_name}](https://trello.com/c/{card_link})" moved from "[{board_name}](https://trello.com/b/{board_link}) to board "{board_target_name}"'
 
         return payload.format(**context)
 
@@ -120,8 +120,9 @@ class Hook(object):
             'board_name': data['board']['name'],
             'board_link': data['board']['shortLink'],
             'card_name': data['card']['name'],
+            'card_link': data['card']['shortLink'],
         }
-        payload = u':incoming_envelope: Card moved: "[{card_name}](https://trello.com/b/{board_link})" moved to "[{board_name}](https://trello.com/b/{board_link}) from board "{board_source_name}"'
+        payload = u':incoming_envelope: Card moved: "[{card_name}](https://trello.com/c/{card_link})" moved to "[{board_name}](https://trello.com/b/{board_link}) from board "{board_source_name}"'
 
         return payload.format(**context)
 
