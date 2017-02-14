a0bb469da098f13132565ed2fa7d024ae969f136
THEBAULT Julien <julien@thebault.co>
2017-02-14 18:13:15 +0100

    Ensure utf8 header is present in all python files

--- matterllo/core/urls.py
+++ matterllo/core/urls.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.conf.urls import url
 
 from matterllo.core.views import (
--- matterllo/core/views/webhook.py
+++ matterllo/core/views/webhook.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.contrib.auth.decorators import login_required
 from django.contrib.messages.views import SuccessMessageMixin
 from django.core.urlresolvers import reverse_lazy
