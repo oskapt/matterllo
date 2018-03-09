407197bf871dbc6788b0e03cfbb7f0fcbdccb722
Adrian Goins <mon@chus.cc>
2018-03-09 10:54:15 -0300

    modified for running with secrets

---
+++ docker-entrypoint.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
+# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
+#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+file_env() {
+    local var="$1"
+    local fileVar="${var}_FILE"
+    local def="${2:-}"
+    if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+        echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+        exit 1
+    fi
+    local val="$def"
+    if [ "${!var:-}" ]; then
+        val="${!var}"
+    elif [ "${!fileVar:-}" ]; then
+        val="$(< "${!fileVar}")"
+    fi
+    export "$var"="$val"
+    unset "$fileVar"
+}
+
+: ${LISTEN_PORT:=8000}
+: ${LISTEN_ADDRESS:=0.0.0.0}
+
+file_env TRELLO_APIKEY
+file_env TRELLO_TOKEN
+
+if [[ -z ${TRELLO_APIKEY} || -z ${TRELLO_TOKEN} ]]; then
+  echo >&2 "ERROR: missing TRELLO_APIKEY or TRELLO_TOKEN"
+  sleep 5
+  exit 1
+fi
+
+if [[ "$1" ]]; then
+  cmd="$@"
+else
+  cmd="python manage.py migrate && python manage.py runserver ${LISTEN_PORT}:${LISTEN_ADDRESS}"
+fi
+
+exec "$@"
+
